083f542fb8959fb82a3663e0c3973be35e2662e7
MyeongGeun Shin <mg.shin@kaist.ac.kr>
2020-06-24 03:04:02 +0900

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-command -v python3 >/dev/null 2>&1 && apt install python3
-command -v python3 -c "import netaddr" >/dev/null 2>&1 && python3 -m pip install netaddr
-command -v python3 -c "from scapy.all import *" >/dev/null 2>&1 && python3 -m pip install scapy
+type -P python3 >/dev/null 2>&1 || apt install python3
+python3 -m pip >/dev/null 2>&1 || apt install python3-pip -y
+python3 -c "import netaddr" >/dev/null 2>&1 || sudo -H python3 -m pip install -U netaddr
+python3 -c "from scapy.all import *" >/dev/null 2>&1 || sudo -H python3 -m pip install -U scapy
+echo "All clear :)"
